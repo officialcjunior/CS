315cb8702af1508212a95b4c9f51f4553c82b2b6
hrishikeshmohan23 <57557526+hrishikeshmohan23@users.noreply.github.com>
2020-04-16 15:14:24 +0530

    Solution to the third quetion
    Pardin me if there are any mistakes in spellings

---
+++ Strings/String-Task-1/3.c
@@ -0,0 +1,36 @@
+/*Author:Hrishikesh P
+  Roll NO:AM.EN.U4CSE19024
+  Class-CSE-A 
+  Program to input a string and check if contains only unique characters.*/
+
+#include<stdio.h>
+#include<string.h> //header file to use string based functions
+#include<stdlib.h>
+int main()
+{
+	char c[100];
+	int f=0;
+	printf("Enter the string:");
+	
+	scanf("%[^\n]%*c", c);
+	
+	int i=0,j=0;
+	for(i=0;i<strlen(c);i++)  //Checking if we have charcters that ae matching
+	{
+		for(j=i+1;j<strlen(c);j++)
+		{
+			if(c[i]==c[j])  //if they are matching then we make our flag variable f 1 and we break from the loops
+			{
+				f=1;
+				break;
+			}
+			if(f)
+			break;
+		}
+	}
+	if(f)
+	printf("NO");
+	else
+	printf("YES");
+
+}

736a4d21b81d62b465ed7fbaafddbb48318a3510
officialcjunior <realc@protonmail.com>
2020-04-27 19:13:22 +0530

    2D-Array-1: Update 4.c, Added functions and basic documentaion

--- Computer-Programming/2D-Array/2D-Array-Task-1/4.c
+++ Computer-Programming/2D-Array/2D-Array-Task-1/4.c
@@ -1,21 +1,12 @@
 #include<stdio.h>
-#include<string.h>
 
-int main()
+//Transforms the given matrix to an upper traingular matrix
+
+void to_upper_triangular(int matrix[][10], int r, int c, int *sum)
 {
-	int r,c,sum=0;
-	puts("Enter the number of rows and columns:");
-	scanf("%d",&r);
-	scanf("%d",&c);
-	int arr[r][c];
-	puts("Enter the matrix: ");
-	for (int i = 0; i < r; ++i)
-	{
-		for (int j = 0; j < c; ++j)
-		{
-			scanf("%d",&arr[i][j]);
-		}
-	}
+
+	//Transforms the given matrix to an upper traingular matrix
+
 
 	for(int i=0 ; i < r ; i++)
 	{
@@ -23,26 +14,57 @@ int main()
 		{
 			if(i > j)
 			{
-				arr[i][j] =0;
+				matrix[i][j] =0;
 			}
 		}
 	}
-	puts("UPPER TRIANGULAR MATRIX: ");
+
+	//Prints the current matrix, that is upper traingular matrix
+
+	puts("Upper triangular matrix: ");
 	for (int i = 0; i < r; ++i)
 	{
 		for (int j = 0; j < c; ++j)
 		{
-			printf("%d ",arr[i][j]);
+			printf("%d ",matrix[i][j]);
 		}
 		printf("\n");
-	}
+	} 
+
+	//Computes the sum of elements
 
 	for (int i = 0; i < r; ++i)
 	{
 		for (int j = 0; j < c; ++j)
 		{
-			sum+=arr[i][j];
+			*sum += matrix[i][j];
 		}
 	}
-	printf("%d\n",sum );
+
+}
+
+int main()
+{
+	int sum=0,r,c,matrix[10][10];
+	printf("Enter the number of rows and columns\n");
+	scanf("%d %d",&r,&c);
+
+	//Assigning elements to the matrix
+
+	printf("Enter the matrix elements\n");
+	for (int i = 0; i < r; ++i)
+	{
+		for (int j = 0; j < c; ++j)
+		{
+			scanf("%d",&matrix[i][j]);
+		}
+	}
+
+	//Calls to_upper_triangular, the variable sum is called by reference
+
+	to_upper_triangular(matrix,r,c,&sum);
+
+	printf("Sum = %d\n",sum );
+	printf("%s \n",sum>100?"YES":"NO");
+
 }

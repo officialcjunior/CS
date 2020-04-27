17056b8f8918d6881bced461d69e37ea02fee024
officialcjunior <realc@protonmail.com>
2020-04-27 18:38:29 +0530

    2D-Array-1: Update 2.c, added functions and basic documentaion

--- Computer-Programming/2D-Array/2D-Array-Task-1/2.c
+++ Computer-Programming/2D-Array/2D-Array-Task-1/2.c
@@ -2,33 +2,44 @@
 
 #include <stdio.h>
 
-int main ()
-	{
 
-		int r,c, sum=0;
-		puts("Enter the number of rows and columns");	
-		scanf("%d %d", &r, &c);
-		int matrix[r][c];
-		puts("Enter the matrix elements");
+//For each row i, j will go through each element of the row and $sum will have the sum 
 
-		//Assigning elements to the matrix
-		
-		for (int i=0;i<r;i++)
-		{	
-			for (int j=0;j<c;j++)
-				scanf("%d", &matrix[i][j]);
+void sum_of_row(int matrix[][10], int r, int c)
+{
+
+	int sum;
+
+	for (int i=0;i<r;i++)
+	{	
+		sum=0;
+		for (int j=0;j<c;j++)
+		{
+			sum += matrix[i][j];
 		}
-
-		//For each row i, j will go through each element of the row and $sum will have the sum 
-
-		for (int i=0;i<r;i++)
-		{	
-			sum=0;
-			for (int j=0;j<c;j++)
-				sum += matrix[i][j];
-			printf("Sum of %d-th row %d ",i+1, sum);
-			puts("");
-		}
-		return 0;	
-		
+		printf("Sum of %d-th row %d \n",i+1, sum);
 	}
+}
+
+int main ()
+{
+    int r,c, matrix[10][10];
+	puts("Enter the number of rows and columns");	
+	scanf("%d %d", &r, &c);
+	puts("Enter the matrix elements");
+
+	//Assigning elements to the matrix
+	
+	for (int i=0;i<r;i++)
+	{	
+		for (int j=0;j<c;j++)
+		{
+			scanf("%d", &matrix[i][j]);
+		}
+	}
+
+	sum_of_row(matrix,r,c);
+
+	return 0;	
+	
+}

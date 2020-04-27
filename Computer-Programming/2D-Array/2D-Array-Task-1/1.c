4fa01b6dcb893e60f64443a15b07d732da88dd74
officialcjunior <realc@protonmail.com>
2020-04-27 18:05:07 +0530

    2D-Array-1: Update 1.c, added functions

--- Computer-Programming/2D-Array/2D-Array-Task-1/1.c
+++ Computer-Programming/2D-Array/2D-Array-Task-1/1.c
@@ -2,41 +2,59 @@
 
 #include <stdio.h>
 
+
+//Assigns the transpose to another two-dimensional array named transpose_matrix
+
+void transpose(int matrix[][10], int transpose_matrix[][10],int r, int c)
+{
+		for (int i=0;i<r;i++)
+		{	
+			for (int j=0;j<c;j++)
+				transpose_matrix[i][j]=matrix[j][i];
+		}
+}
+
+
+//Printing the transpose to STDOUT
+
+
+void displaymatrix(int transpose_matrix[][10], int r, int c)
+{
+
+		puts("The transpose is :");
+
+		for (int i=0;i<r;i++)
+		{	
+			for (int j=0;j<c;j++)
+				printf("%d ", transpose_matrix[i][j]);
+			puts("");
+		}
+
+}
+
 int main ()
 	{
 
-		int n;
-		puts("Enter the order of the matrix");	
-		scanf("%d", &n);
-		int matrix[n][n],transpose[n][n];
+	    int r,c, matrix[10][10],transpose_matrix[10][10];
+		puts("Enter the number of rows and columns");	
+		scanf("%d %d", &r, &c);
+
 		puts("Enter the matrix elements");
 
 		//Assigning elements to the matrix
 		
-		for (int i=0;i<n;i++)
+		for (int i=0;i<r;i++)
 		{	
-			for (int j=0;j<n;j++)
+			for (int j=0;j<c;j++)
 				scanf("%d", &matrix[i][j]);
 		}
 
-		//Assigning the transpose to another two-dimensional array named transpose
+		transpose(matrix,transpose_matrix,r,c);
 
-		for (int i=0;i<n;i++)
-		{	
-			for (int j=0;j<n;j++)
-				transpose[i][j]=matrix[j][i];
-		}
 
-		puts("The transpose is :");
+		displaymatrix(transpose_matrix,r,c);
 		
-		//Printing the transpose to STDOUT
-
-		for (int i=0;i<n;i++)
-		{	
-			for (int j=0;j<n;j++)
-				printf("%d ", transpose[i][j]);
-			puts("");
-		}
 		return 0;	
 		
 	}
+

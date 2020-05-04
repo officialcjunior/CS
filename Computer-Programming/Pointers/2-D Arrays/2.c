d093c073a7824b4269e397ac8c65aa3fe05d68ea
Abijith Prasanthan <52949431+AbijithPrasanthan@users.noreply.github.com>
2020-05-04 21:01:22 +0530

    Question 2

---
+++ Computer-Programming/Pointers/2-D Arrays/2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int search_use_ptr(int *ptr,int n,int x){
+	int flag=0;
+	int *ini = ptr;
+	for(int i=0;i<n;i++){
+		if(*ptr == x){
+			printf("The element is found at position: %d",i+1);
+			flag=1;
+			break;
+		}
+		ptr++;
+		
+	}
+	if(!flag){
+		printf("The element is not found!!!\n");
+	}
+	return 0;
+}
+
+int main(){
+	int arr[50],n,x;
+	int *ptr = arr;
+	printf("Enter the size of the array:\n");
+	scanf("%d",&n);
+	printf("Enter the array\n");
+	for(int i=0;i<n;i++){
+		scanf("%d",ptr);
+		ptr++;
+	}
+	printf("Enter the element to be searched :\n");
+	scanf("%d",&x);
+	search_use_ptr(arr,n,x);
+}

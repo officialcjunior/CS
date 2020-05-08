0ab374c9aa6beff6f8812c966a70e4c6e00c535b
Abijith Prasanthan <52949431+AbijithPrasanthan@users.noreply.github.com>
2020-05-08 15:24:31 +0530

    made some changes to the script

--- User-Interface-Design/JS/question-9/script.js
+++ User-Interface-Design/JS/question-9/script.js
@@ -1,5 +1,6 @@
 function display_info(){
 	var first_name=document.querySelector("#first").value;
 	var last_name=document.querySelector("#last").value
-	document.getElementById("display").innerHTML = "<h2>Hello " + first_name + " " + last_name + "!!</h2>";
+	/*document.getElementById("display").innerHTML = "<h2>Hello " + first_name + " " + last_name + "!!</h2>";*/
+	alert("first_name" + " " + "last_name");
 }

33cb5ca0ff4d5a4e390afd18897a1121970f0963
Abijith Prasanthan <52949431+AbijithPrasanthan@users.noreply.github.com>
2020-05-08 15:25:30 +0530

    Corrected some errors

--- User-Interface-Design/JS/question-9/script.js
+++ User-Interface-Design/JS/question-9/script.js
@@ -2,5 +2,5 @@ function display_info(){
 	var first_name=document.querySelector("#first").value;
 	var last_name=document.querySelector("#last").value
 	/*document.getElementById("display").innerHTML = "<h2>Hello " + first_name + " " + last_name + "!!</h2>";*/
-	alert("first_name" + " " + "last_name");
+	alert(first_name + " " + last_name);
 }

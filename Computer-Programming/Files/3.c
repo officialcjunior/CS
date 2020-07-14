f07cf45bdef07844aa800a47c7d511ca4c093c8a
hrishikeshmohan23 <57557526+hrishikeshmohan23@users.noreply.github.com>
2020-07-14 21:24:24 +0530

    Adding solution to 3rd problem
    If any problems found do commit the changes

---
+++ Computer-Programming/Files/3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	FILE *f;
+	f=fopen("names.txt","r");
+	if(f==NULL)
+		printf("The file didn't open!!");
+	else
+	{
+		int c1=0,c2=0;
+		
+		char ch=fgetc(f);
+		while(ch!=EOF)
+		{
+			printf("%c",ch);
+			if(ch==' ' || ch=='\n')
+				c1++;
+			else
+				c2++;
+			ch=fgetc(f);
+		}
+		printf("\nNo of words=%d\nNo of characters=%d\n",c1,c2-1);
+		}
+		fclose(f);
+	return 0;
+}
